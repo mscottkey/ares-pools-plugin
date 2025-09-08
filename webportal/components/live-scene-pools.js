65f4edb35f7dc7f4050df90c1b21f492abdb1abc
mscottkey <mscottkey@gmail.com>
2025-09-08 06:02:09 +0000

    Updates to use hybrid ember approach, no glimmer.

--- webportal/components/live-scene-pools.js
+++ webportal/components/live-scene-pools.js
@@ -1,176 +1,144 @@
-import Component from '@glimmer/component';
+import Component from '@ember/component';
 import { inject as service } from '@ember/service';
 import { action } from '@ember/object';
-import { tracked } from '@glimmer/tracking';
 
-export default class LiveScenePoolsComponent extends Component {
-  @service gameApi;
-  @service flashMessages;
-
-  @tracked showPool = false;
-  @tracked selectSpendPool = false;
-  @tracked selectAddPool = false;
-  @tracked selectResetPool = false;
-  @tracked selectShowPool = false;
-  @tracked poolSpendReason = null;
-  @tracked poolAddReason = null;
-  @tracked poolAddAmount = null;
-  @tracked poolSpendAmount = null;
-
-  get scene() {
-    return this.args.scene;
-  }
-
-  get poseChar() {
-    return this.scene && this.scene.poseChar;
-  }
-
-  @action
-  updateProperty(property, event) {
-    this[property] = event.target.value;
-  }
+export default Component.extend({
+  tagName: '',
+  gameApi: service(),
+  flashMessages: service(),
+  
+  showPool: false,
+  selectSpendPool: false,
+  selectAddPool: false,
+  selectResetPool: false,
+  selectShowPool: false,
+  poolSpendReason: null,
+  poolAddReason: null,
+  poolAddAmount: null,
+  poolSpendAmount: null,
 
   @action
   spendPool() {
-    if (!this.poseChar) {
-      this.flashMessages.danger("Scene data is not available.");
-      return;
-    }
-
-    if (!this.poolSpendReason) {
+    let api = this.gameApi;
+    let poolSpendReason = this.poolSpendReason;
+    let poolSpendAmount = this.poolSpendAmount;
+  
+    this.set('selectSpendPool', false);
+    this.set('poolSpendReason', null);
+    this.set('poolSpendAmount', null);
+        
+    if (!poolSpendReason) {
       this.flashMessages.danger("You haven't given a reason for your Pool spend.");
       return;
     }
 
-    if (!this.poolSpendAmount) {
+    if (!poolSpendAmount) {
       this.flashMessages.danger("You haven't specified a value for your Pool spend.");
       return;
     }
     
-    this.gameApi.requestOne('spendPool', { 
-      scene_id: this.scene.id,
-      reason: this.poolSpendReason, 
-      amount: this.poolSpendAmount, 
-      sender: this.poseChar.name 
+    api.requestOne('spendPool', { 
+      scene_id: this.get('scene.id'),
+      reason: poolSpendReason, 
+      amount: poolSpendAmount, 
+      sender: this.get('scene.poseChar.name') 
     }, null)
     .then( (response) => {
       if (response.error) {
         console.log(response.error);
         return;
       }
-      this.selectSpendPool = false;
-      this.poolSpendReason = null;
-      this.poolSpendAmount = null;
     });
-  }
+  },
 
   @action
   addPool() {
-    if (!this.poseChar) {
-      this.flashMessages.danger("Scene data is not available.");
-      return;
-    }
+    let api = this.gameApi;
+    let poolAddReason = this.poolAddReason;
+    let poolAddAmount = this.poolAddAmount;
+  
+    this.set('selectAddPool', false);
+    this.set('poolAddAmount', null);
+    this.set('poolAddReason', null);
 
-    if (!this.poolAddAmount) {
+    if (!poolAddAmount) {
       this.flashMessages.danger("You haven't given a value for your pool add.");
       return;
     }
         
-    if (!this.poolAddReason) {
+    if (!poolAddReason) {
       this.flashMessages.danger("You haven't given a reason for adding to your pool.");
       return;
     }
 
-    this.gameApi.requestOne('addPool', { 
-      scene_id: this.scene.id,
-      reason: this.poolAddReason, 
-      amount: this.poolAddAmount, 
-      sender: this.poseChar.name 
+    api.requestOne('addPool', { 
+      scene_id: this.get('scene.id'),
+      reason: poolAddReason, 
+      amount: poolAddAmount, 
+      sender: this.get('scene.poseChar.name') 
     }, null)
     .then( (response) => {
       if (response.error) {
         return;
       }
-      this.selectAddPool = false;
-      this.poolAddAmount = null;
-      this.poolAddReason = null;
     });
-  }
+  },
 
   @action
   resetPool() {
-    if (!this.poseChar) {
-      this.flashMessages.danger("Scene data is not available.");
-      return;
-    }
+    let api = this.gameApi;
+  
+    this.set('selectResetPool', false);
 
-    this.gameApi.requestOne('resetPool', { 
-      scene_id: this.scene.id,
-      sender: this.poseChar.name 
+    api.requestOne('resetPool', { 
+      scene_id: this.get('scene.id'),
+      sender: this.get('scene.poseChar.name') 
     }, null)
     .then( (response) => {
       if (response.error) {
         return;
       }
-      this.selectResetPool = false;
     });
-  }
+  },
 
   @action
   desperatePool() {
-    if (!this.poseChar) {
-      this.flashMessages.danger("Scene data is not available.");
-      return;
-    }
+    let api = this.gameApi;
 
-    this.gameApi.requestOne('desperatePool', { 
-      scene_id: this.scene.id,
-      sender: this.poseChar.name 
+    api.requestOne('desperatePool', { 
+      scene_id: this.get('scene.id'),
+      sender: this.get('scene.poseChar.name') 
     }, null)
     .then( (response) => {
       if (response.error) {
         return;
       }
     });
-  }
+  },
 
   @action
   showPool() {
-    if (!this.poseChar) {
-      this.flashMessages.danger("Scene data is not available.");
-      return;
-    }
+    let api = this.gameApi;
     
-    this.gameApi.requestOne('showPool', { 
-      scene_id: this.scene.id,
-      sender: this.poseChar.name 
+    api.requestOne('showPool', { 
+      scene_id: this.get('scene.id'),
+      sender: this.get('scene.poseChar.name') 
     }, null)
     .then( (response) => {
       if (response.error) {
         console.log(response.error);
         return;
       }
-      this.selectShowPool = false;
     });
-  }
+  },
 
   @action
   setSelectSpendPool(value) {
-    this.selectSpendPool = value;
-  }
-
-  @action
-  setSelectSpendPoolFalse() {
-    this.selectSpendPool = false;
-  }
+    this.set('selectSpendPool', value);
+  },
 
   @action
   setSelectAddPool(value) {
-    this.selectAddPool = value;
+    this.set('selectAddPool', value);
   }
-
-  @action
-  setSelectAddPoolFalse() {
-    this.selectAddPool = false;
-  }
-}
+});
